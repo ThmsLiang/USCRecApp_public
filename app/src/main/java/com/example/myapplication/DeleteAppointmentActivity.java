0ade265695c5d6e92b0480185984a69e0a83b9b1
Eli Morris <elimorri@usc.edu>
2022-04-11 22:57:52 -0700

    API key updated

--- app/src/main/java/com/example/myapplication/DeleteAppointmentActivity.java
+++ app/src/main/java/com/example/myapplication/DeleteAppointmentActivity.java
@@ -90,7 +90,7 @@ public class DeleteAppointmentActivity extends AppCompatActivity {
                         }
                     }
                 });
-                SendGrid sendGrid = SendGrid.create("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");
+                SendGrid sendGrid = SendGrid.create("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");
 
                 SendGridMail testMail = new SendGridMail();
                 testMail.setFrom("elimorri@usc.edu",null);
